3b87424ea2179aaddda3ae1b17b0af9f7b67e5c5
wuxinhao-eric <wuxinhao0215@163.com>
2019-09-10 15:39:42 +0800

    add sensitiviti and specificity

--- icbc_GBDT.py
+++ icbc_GBDT.py
@@ -12,6 +12,19 @@ print(gdbt.score(X_train,y_train))
 gdbt_predict = gdbt.predict(X_test)
 print(gdbt.score(X_test,y_test))
 
+
+from sklearn import metrics
+# 行是实际，列是预测
+cm = metrics.confusion_matrix(y_test,gdbt_predict,labels=[0,1])
+print(cm)
+
+Accuracy = metrics.scorer.accuracy_score(y_test,gdbt_predict)
+Sensitivity = metrics.scorer.recall_score(y_test,gdbt_predict)
+Specificity = metrics.scorer.recall_score(y_test,gdbt_predict,pos_label=0)
+print('模型准确率：',Accuracy*100)
+print('正例覆盖率：',Sensitivity*100)
+print('负例覆盖率：',Specificity*100)
+'''
 from sklearn import metrics
 import matplotlib.pyplot as plt
 fpr , tpr , _= metrics.roc_curve(y_test,gdbt.predict_proba(X_test)[:,1])
@@ -20,4 +33,4 @@ plt.stackplot(fpr,tpr,color='steelblue')
 plt.plot([0,1],[0,1],linestyle='dashed', color='black')
 plt.text(0.6,0.4,'AUG=%.3f' % metrics.auc(fpr,tpr),fontdict=dict(size=18))
 plt.show()
-print('s')
+'''
